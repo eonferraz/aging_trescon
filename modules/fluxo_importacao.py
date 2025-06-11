20267048c2e2551cff1405d3e00c6b3203bf0e74
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-06-11 16:24:53 -0300

    Create fluxo_importacao.py

---
+++ modules/fluxo_importacao.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from modules import importar_titulos, extrator_titulos
+
+def executar():
+    st.markdown("<div class='custom-subheader'>1️⃣ Importação dos Títulos Financeiros</div>", unsafe_allow_html=True)
+    importar_titulos.executar()
+
+    if "df_titulos" in st.session_state:
+        df = st.session_state["df_titulos"]
+        st.markdown("<div class='custom-subheader'>2️⃣ Extração e Mapeamento de Campos</div>", unsafe_allow_html=True)
+        extrator_titulos.executar(df)
