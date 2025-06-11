72cd095af070df867e21dbaf4195b45dd9ee22e4
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-06-11 15:07:06 -0300

    Update cabecalho.py

--- utils/cabecalho.py
+++ utils/cabecalho.py
@@ -8,9 +8,9 @@ def exibir_cabecalho():
         f"""
         <div class="header">
             <div style="display: flex; align-items: center; gap: 15px;">
-                <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=304,fit=crop,q=95/Aq2B471lDpFnv1BK/logo---trescon-30-anos-mv0jg6Lo2EiV7yLp.png" style="height: 50px;">
+                <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=304,fit=crop,q=95/Aq2B471lDpFnv1BK/logo---trescon-30-anos-mv0jg6Lo2EiV7yLp.png" style="height: 30px;">
                 <div>
-                    <h1 style="margin: 0; font-size: 1.8em;">Relatório de Aging - Conciliador</h1>
+                    <h1 style="margin: 0; font-size: 1.0em;">Relatório de Aging - Conciliador</h1>
                     <p style="margin: 0; color: #666;">Versão 1.1 - {data_formatada}</p>
                 </div>
             </div>
