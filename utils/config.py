72a9513e60ca74064a7640627c677b8cce3376e8
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-06-11 14:44:30 -0300

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def configurar_pagina():
+    st.set_page_config(
+        page_title="Relatório de Aging - Conciliador",
+        page_icon="📊",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )  
