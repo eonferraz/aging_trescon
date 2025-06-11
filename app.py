9c6108ef29eed6d61c2411006b8f2e24e58152d8
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-06-11 14:39:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,13 @@ import streamlit as st
 from datetime import datetime
 from PIL import Image
 from utils.style import aplicar_css
+from utils.cabecalho import exibir_cabecalho
 
 # Importar módulos (conforme forem sendo criados)
 from modules import upload_dados, conciliacao, resumo  # exemplo
 
-# Cabeçalho com logo e data
-st.markdown(
-    """
-    <div class="header">
-        <div style="display: flex; align-items: center; gap: 15px;">
-            <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=304,fit=crop,q=95/Aq2B471lDpFnv1BK/logo---trescon-30-anos-mv0jg6Lo2EiV7yLp.png" style="height: 50px;">
-            <div>
-                <h1 style="margin: 0; font-size: 1.8em;">Relatório de Aging - Conciliador</h1>
-                <p style="margin: 0; color: #666;">Versão 1.1 - {date}</p>
-            </div>
-        </div>
-    </div>
-    """.format(date=datetime.now().strftime("%d/%m/%Y")),
-    unsafe_allow_html=True
-)
+#Chama a função com logo e data
+exibir_cabecalho()
 
 #Chama a aplicação de CSS que está em utils
 aplicar_css()
