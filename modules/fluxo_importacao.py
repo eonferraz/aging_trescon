44b3c3f93db33607dd873905a4c1259621c90fe8
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-06-11 16:27:28 -0300

    Update fluxo_importacao.py

--- modules/fluxo_importacao.py
+++ modules/fluxo_importacao.py
@@ -2,10 +2,10 @@ import streamlit as st
 from modules import importar_titulos, extrator_titulos
 
 def executar():
-    st.markdown("<div class='custom-subheader'>1️⃣ Importação dos Títulos Financeiros</div>", unsafe_allow_html=True)
+    st.markdown("<div class='custom-subheader'>1.1 Importação do Arquivo</div>", unsafe_allow_html=True)
     importar_titulos.executar()
 
     if "df_titulos" in st.session_state:
         df = st.session_state["df_titulos"]
-        st.markdown("<div class='custom-subheader'>2️⃣ Extração e Mapeamento de Campos</div>", unsafe_allow_html=True)
+        st.markdown("<div class='custom-subheader'>Extração e Mapeamento de Campos</div>", unsafe_allow_html=True)
         extrator_titulos.executar(df)
