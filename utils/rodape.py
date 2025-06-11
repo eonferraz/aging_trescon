60e7303a9fdd4d6132232f5958d7a501336e014f
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-06-11 15:06:39 -0300

    Update rodape.py

--- utils/rodape.py
+++ utils/rodape.py
@@ -7,8 +7,8 @@ def exibir_rodape():
 
     st.markdown(
         f"""
-        <hr style="margin-top: 50px; margin-bottom: 20px;">
-        <div style="text-align: center; font-size: 0.85em; color: #999;">
+        <hr style="margin-top: 10px; margin-bottom: 5px;">
+        <div style="text-align: center; font-size: 0.5em; color: #999;">
             Sistema desenvolvido por Paulo Ferraz para <strong>{EMPRESA}</strong> — Versão {VERSAO_APP}<br>
             Última execução: {data_hora}
         </div>
