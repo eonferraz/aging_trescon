21055c8c8e3f5185d4cc55f49eddfdf54808b42e
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-06-12 01:39:44 -0300

    Update fluxo_exportacao.py

--- modules/fluxo_exportacao.py
+++ modules/fluxo_exportacao.py
@@ -3,7 +3,7 @@ import pandas as pd
 import io
 import streamlit as st
 
-def exportar_relatorio_conciliacao(df: pd.DataFrame):
+def executar(df: pd.DataFrame):
     output = io.BytesIO()
     with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
         df.to_excel(writer, index=False, sheet_name="Conciliação")
