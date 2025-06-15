f44f76547ad58b6007d1d34b1e941ee0af5bdf7d
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-06-15 19:21:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,16 +18,30 @@ from utils.rodape import exibir_rodape
 # from utils.menu_etapas import exibir_menu_lateral
 
 #Importação dos modulos
+# from modules import (
+#     fluxo_importacao_titulos,
+#     fluxo_extracao_titulos,
+#     fluxo_importacao_baixas,
+#     fluxo_extracao_baixas,
+#     fluxo_extracao_conciliado,
+#     fluxo_importacao_conciliado,
+#     fluxo_conciliacao,
+#     fluxo_exportacao
+# )
+
 from modules import (
+    fluxo_importacao_conciliado,
+    fluxo_extracao_conciliado,
     fluxo_importacao_titulos,
     fluxo_extracao_titulos,
+    fluxo_unificacao_titulos_conciliado,
     fluxo_importacao_baixas,
     fluxo_extracao_baixas,
-    fluxo_extracao_conciliado,
-    fluxo_importacao_conciliado,
     fluxo_conciliacao,
     fluxo_exportacao
 )
+
+
 
 import streamlit as st
 
@@ -49,23 +63,33 @@ aplicar_css()
 # ETAPAS EM FORMATO EXPANSÍVEL
 #=======================================================================================================================================
 with st.expander("1️⃣ Conciliação Anterior", expanded=True):
-    fluxo_importacao_titulos.executar()
-    if "df_conciliacao" in st.session_state:
-        fluxo_extracao_conciliacao.executar()
+    fluxo_importacao_conciliado.executar()
+    fluxo_extracao_conciliado.executar()
 
 with st.expander("2️⃣ Títulos Novos"):
     fluxo_importacao_titulos.executar()
-    if "df_titulos" in st.session_state:
-        fluxo_extracao_titulos.executar()
+    fluxo_extracao_titulos.executar()
+    # if "df_titulos" in st.session_state:
+    #     fluxo_extracao_titulos.executar()
 
-with st.expander("3️⃣ Baixas"):
-    if "df_titulos" in st.session_state:
-        fluxo_importacao_baixas.executar()
-        fluxo_extracao_baixas.executar()
-        
-with st.expander("[4] Conciliação"):
-    if "df_baixas" in st.session_state and "df_titulos" in st.session_state:
-        fluxo_conciliacao.executar()
+with st.expander("3️⃣ Unificar Títulos + Conciliação Anterior"):
+    fluxo_unificacao_titulos_conciliado.executar()
+
+
+with st.expander("4️⃣ Baixas"):
+    fluxo_importacao_baixas.executar()
+    fluxo_extracao_baixas.executar()
+    # if "df_titulos" in st.session_state:
+    #     fluxo_importacao_baixas.executar()
+    #     fluxo_extracao_baixas.executar()
+
+
+with st.expander("5️⃣ Conciliação"):
+    fluxo_conciliacao.executar()
+    fluxo_exportacao.executar()
+    
+    # if "df_baixas" in st.session_state and "df_titulos" in st.session_state:
+    #     fluxo_conciliacao.executar()
 #=======================================================================================================================================
 
 
